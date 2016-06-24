61c7d59e9950b9e894510f248e4c4a3c061803b1
Maxuz <maksimandr@mail.ru>
2016-06-24 13:32:50 +0500

    add general components in linearlayout

---
+++ app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
@@ -0,0 +1,9 @@
+package com.softdesign.devintensive.utils;
+
+/**
+ * Содержит константы
+ * Created by MKS-PC on 23.06.2016.
+ */
+public interface ConstantManager {
+    String TAG_PREFIX="DEV ";
+}
