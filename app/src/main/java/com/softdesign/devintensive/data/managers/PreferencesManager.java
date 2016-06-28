79c66e8d3019e9065a995487dbe349af49ef35fe
Maxuz <maksimandr@mail.ru>
2016-06-28 23:56:43 +0500

    Hometask_3 completed

---
+++ app/src/main/java/com/softdesign/devintensive/data/managers/PreferencesManager.java
@@ -0,0 +1,40 @@
+package com.softdesign.devintensive.data.managers;
+
+import android.content.SharedPreferences;
+
+import com.softdesign.devintensive.utils.ConstantManager;
+import com.softdesign.devintensive.utils.DevintensiveAplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by MKS-PC on 28.06.2016.
+ */
+public class PreferencesManager {
+
+    private SharedPreferences mSharedPreferences;
+    private static final String[] USER_FIELDS = {ConstantManager.USER_PHONE_KEY, ConstantManager.USER_EMAIL_KEY, ConstantManager.USER_VK_KEY, ConstantManager.USER_GIT_KEY, ConstantManager.USER_BIO_KEY};
+
+    public PreferencesManager() {
+        mSharedPreferences = DevintensiveAplication.getSharedPreferences();
+    }
+
+    public void saveUserProfileData(List<String> userFields) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+
+        for (int i = 0; i < USER_FIELDS.length; i++) {
+            editor.putString(USER_FIELDS[i], userFields.get(i));
+        }
+        editor.apply();
+    }
+
+    public List<String> loadUserProfileData() {
+        List<String> userFields = new ArrayList<>();
+        for (int i = 0; i < USER_FIELDS.length; i++) {
+            userFields.add(mSharedPreferences.getString(USER_FIELDS[i], null));
+        }
+        return userFields;
+    }
+
+}
--- app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
+++ app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
@@ -5,5 +5,13 @@ package com.softdesign.devintensive.utils;
  * Created by MKS-PC on 23.06.2016.
  */
 public interface ConstantManager {
-    String TAG_PREFIX="DEV ";
+    String TAG_PREFIX = "DEV ";
+    String COLOR_MODE_KEY = "COLOR_MODE_KEY";
+    String EDIT_MODE_KEY = "EDIT_MODE_KEY";
+
+    String USER_PHONE_KEY = "USER_KEY_1";
+    String USER_EMAIL_KEY = "USER_KEY_2";
+    String USER_VK_KEY = "USER_KEY_3";
+    String USER_GIT_KEY = "USER_KEY_4";
+    String USER_BIO_KEY = "USER_KEY_5";
 }
